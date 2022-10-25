9eb479d373c3716b83654a731018bb3422af83b1
marcos.gonzalez <marcos.gonzalez@imatia.com>
2022-10-25 11:28:20 +0200

    Angular auth app with guards and JWT security

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component:MainComponent,
+    children:[
+      {path:"login",component:LoginComponent},
+      {path:"register",component:RegisterComponent},
+      {path:"**",redirectTo:"login"}
+    ]
+  },
+  {
+    path: "dashboard",
+    loadChildren: () => import("./auth.module").then(m => m.AuthModule)
+  },
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
